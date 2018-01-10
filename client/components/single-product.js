9279ff8cc25483212e2a2bfbfd271753c86c2b30
Randy Tsao <randytsao24@gmail.com>
2018-01-10 16:04:16 -0500

    Changed single product component to stateless React component, added backend for selected product fetching

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -1,6 +1,6 @@
 // components/single-product.js
 
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -9,49 +9,95 @@ import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import { getSelectedProduct } from '../store/selected-product';
+import store from '../store';
 
 /**
  * COMPONENT
  */
-export const SingleProduct = (props) => {
+// export const SingleProduct = (props) => {
 
-  return props.products && (
-    <div>
-    	<Card>
-		    <CardHeader
-		      title="URL Avatar"
-		      subtitle="Subtitle"
-		      avatar="images/jsa-128.jpg"
-		    />
-		    <CardMedia
-		      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-		    >
-		      <img src={product.imageUrl} alt="" />
-		    </CardMedia>
-		    <CardTitle title="Card title" subtitle="Card subtitle" />
-		    <CardText>
-		      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-		      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-		      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-		      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-		    </CardText>
-		    <CardActions>
-		      <FlatButton label="Action1" />
-		      <FlatButton label="Action2" />
-		    </CardActions>
-		  </Card>
-    </div>
-  );
-};
+//   return props.products && (
+//     <div>
+//     	<Card>
+// 		    <CardHeader
+// 		      title="URL Avatar"
+// 		      subtitle="Subtitle"
+// 		      avatar="images/jsa-128.jpg"
+// 		    />
+// 		    <CardMedia
+// 		      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+// 		    >
+// 		      <img src={product.imageUrl} alt="" />
+// 		    </CardMedia>
+// 		    <CardTitle title="Card title" subtitle="Card subtitle" />
+// 		    <CardText>
+// 		      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// 		      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+// 		      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+// 		      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+// 		    </CardText>
+// 		    <CardActions>
+// 		      <FlatButton label="Action1" />
+// 		      <FlatButton label="Action2" />
+// 		    </CardActions>
+// 		  </Card>
+//     </div>
+//   );
+// };
+
+class SingleProduct extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		const productId = this.props.match.params.id;
+		
+		this.props.loadSelectedProduct(productId);
+	}
+
+	render() {
+	  return this.props.product && (
+	    <div>
+	    	<Card>
+			    <CardHeader
+			      title={this.props.product.name}
+			      subtitle="PLACEHOLDER CATEGORY AND RATING"
+			    />
+			    <CardMedia>
+			      <img src={this.props.product.imageUrl} />
+			    </CardMedia>
+			    <CardTitle title={this.props.product.name} subtitle="PLACEHOLDER" />
+			    <CardText>
+			      {this.props.product.description}
+			    </CardText>
+			    <CardActions>
+			      <FlatButton label="Action1" />
+			      <FlatButton label="Action2" />
+			    </CardActions>
+			  </Card>
+	    </div>
+	  );
+	}
+}
 
 /**
  * CONTAINER
  */
 const mapStateToProps = (state) => {
   return {
-  	products: state.products,
     product: state.selectedProduct
   };
 };
 
-export default connect(mapStateToProps)(SingleProduct)
+const mapDispatchToProps = (dispatch) => {
+	return {
+		loadSelectedProduct(productId) {
+			dispatch(getSelectedProduct(productId));
+		}
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
