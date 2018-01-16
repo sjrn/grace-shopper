5dc6f852f3366f4c0282e440437e2d86d7feac8c
Randy Tsao <randytsao24@gmail.com>
2018-01-16 11:37:56 -0500

    Added text fields for adding review dialog

--- server/index.js
+++ server/index.js
@@ -100,7 +100,7 @@ const startListening = () => {
 }
 
 const syncDb = () => db.sync()
-// const syncDb = () => db.sync({force: true})
+//const syncDb = () => db.sync({force: true})
 
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
